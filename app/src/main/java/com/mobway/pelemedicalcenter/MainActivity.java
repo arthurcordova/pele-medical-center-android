da26ad89799f2b335ac4a3082605ebcee5712023
arthurcordova <arthur.stapassoli@gmail.com>
2017-11-01 13:35:04 -0200

    Bottom style.

--- app/src/main/java/com/mobway/pelemedicalcenter/MainActivity.java
+++ app/src/main/java/com/mobway/pelemedicalcenter/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.mobway.pelemedicalcenter.utils.BottomNavUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
@@ -17,15 +19,19 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_doctor:
+                    mTextMessage.setText(R.string.title_nav_doctors);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_schedule:
+                    mTextMessage.setText(R.string.title_nav_schedules);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_nav_notifications);
                     return true;
+                case R.id.navigation_my_data:
+                    mTextMessage.setText(R.string.title_nav_my_data);
+                    return true;
+
             }
             return false;
         }
@@ -36,8 +42,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        BottomNavUtils.disableShiftMode(navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
