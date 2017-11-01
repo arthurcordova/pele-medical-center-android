005537d81dff213e92e0682d7ffac97daabb104f
arthurcordova <arthur.stapassoli@gmail.com>
2017-11-01 13:52:22 -0200

    Fragments.

--- app/src/main/java/com/mobway/pelemedicalcenter/MainActivity.java
+++ app/src/main/java/com/mobway/pelemedicalcenter/MainActivity.java
@@ -3,15 +3,17 @@ package com.mobway.pelemedicalcenter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
+import com.mobway.pelemedicalcenter.fragments.DoctorFragment;
+import com.mobway.pelemedicalcenter.fragments.NotificationsFragment;
+import com.mobway.pelemedicalcenter.fragments.ProfileFragment;
+import com.mobway.pelemedicalcenter.fragments.ScheduleFragment;
 import com.mobway.pelemedicalcenter.utils.BottomNavUtils;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,18 +22,17 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_doctor:
-                    mTextMessage.setText(R.string.title_nav_doctors);
+                    replaceFragment(new DoctorFragment());
                     return true;
                 case R.id.navigation_schedule:
-                    mTextMessage.setText(R.string.title_nav_schedules);
+                    replaceFragment(new ScheduleFragment());
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_nav_notifications);
+                    replaceFragment(new NotificationsFragment());
                     return true;
                 case R.id.navigation_my_data:
-                    mTextMessage.setText(R.string.title_nav_my_data);
+                    replaceFragment(new ProfileFragment());
                     return true;
-
             }
             return false;
         }
@@ -42,10 +43,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = findViewById(R.id.message);
         BottomNavigationView navigation = findViewById(R.id.navigation);
         BottomNavUtils.disableShiftMode(navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    protected void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, fragment).commit();
+    }
 }
