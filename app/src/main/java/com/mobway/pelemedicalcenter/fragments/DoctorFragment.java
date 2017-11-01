318d957f3ebb83805a4fa0e28761338fad7e7838
arthurcordova <arthur.stapassoli@gmail.com>
2017-11-01 16:36:24 -0200

    Doctors list.

--- app/src/main/java/com/mobway/pelemedicalcenter/fragments/DoctorFragment.java
+++ app/src/main/java/com/mobway/pelemedicalcenter/fragments/DoctorFragment.java
@@ -4,29 +4,20 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mobway.pelemedicalcenter.R;
+import com.mobway.pelemedicalcenter.adapters.RVAdapterDoctor;
+import com.mobway.pelemedicalcenter.models.Doctor;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DoctorFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link DoctorFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class DoctorFragment extends android.support.v4.app.Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,20 +25,9 @@ public class DoctorFragment extends android.support.v4.app.Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DoctorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static DoctorFragment newInstance(String param1, String param2) {
         DoctorFragment fragment = new DoctorFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,19 +36,34 @@ public class DoctorFragment extends android.support.v4.app.Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doctor, container, false);
+        View view = inflater.inflate(R.layout.fragment_doctor, container, false);
+
+        // MOCK ONLY
+        List<Doctor> doctors = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            Doctor d1 = new Doctor();
+            d1.setName("Arthur Cordova Stapassoli");
+            doctors.add(d1);
+        }
+        // MOCK ONLY
+
+        RVAdapterDoctor adapterDoctor = new RVAdapterDoctor(doctors);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_doctor);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapterDoctor);
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -91,16 +86,6 @@ public class DoctorFragment extends android.support.v4.app.Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
