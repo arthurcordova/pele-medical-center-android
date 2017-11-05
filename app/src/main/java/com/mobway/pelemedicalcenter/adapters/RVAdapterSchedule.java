aa34ac19bbd394150e587cfcf2413aadc2f0f323
arthurcordova <arthur.stapassoli@gmail.com>
2017-11-05 16:42:01 -0200

    Schedule list.

---
+++ app/src/main/java/com/mobway/pelemedicalcenter/adapters/RVAdapterSchedule.java
@@ -0,0 +1,66 @@
+package com.mobway.pelemedicalcenter.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.mobway.pelemedicalcenter.R;
+import com.mobway.pelemedicalcenter.models.Doctor;
+
+import java.util.List;
+
+/**
+ * Created by arthur.stapassoli on 01/11/2017.
+ */
+
+public class RVAdapterSchedule extends RecyclerView.Adapter<RVAdapterSchedule.ViewHolder> {
+
+    private List<Doctor> mList;
+
+    public RVAdapterSchedule(List<Doctor> list) {
+        mList = list;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLayoutView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_doctor, null);
+        ViewHolder viewHolder = new ViewHolder(itemLayoutView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int index) {
+        final Doctor model = mList.get(index);
+
+//        holder.tvName.setText(model.getName());
+//        Picasso.with(holder.imgCategory.getContext())
+//                .load(model.getImagem())
+//                .into(holder.imgCategory);
+    }
+
+    public void setFilter(List<Doctor> list) {
+        if (!mList.isEmpty()) {
+            mList.clear();
+        }
+        mList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return (mList != null) ? mList.size() : 0;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvName;
+
+        ViewHolder(View view) {
+            super(view);
+//            tvName = view.findViewById(R.id.tv_name);
+        }
+    }
+}
--- app/src/main/java/com/mobway/pelemedicalcenter/fragments/ScheduleFragment.java
+++ app/src/main/java/com/mobway/pelemedicalcenter/fragments/ScheduleFragment.java
@@ -4,27 +4,25 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mobway.pelemedicalcenter.R;
+import com.mobway.pelemedicalcenter.adapters.RVAdapterDoctor;
+import com.mobway.pelemedicalcenter.models.Doctor;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ScheduleFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ScheduleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class ScheduleFragment extends android.support.v4.app.Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -34,15 +32,7 @@ public class ScheduleFragment extends android.support.v4.app.Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScheduleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static ScheduleFragment newInstance(String param1, String param2) {
         ScheduleFragment fragment = new ScheduleFragment();
         Bundle args = new Bundle();
@@ -64,8 +54,24 @@ public class ScheduleFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        // MOCK ONLY
+        List<Doctor> doctors = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            Doctor d1 = new Doctor();
+            doctors.add(d1);
+        }
+        // MOCK ONLY
+
+        RVAdapterDoctor adapterDoctor = new RVAdapterDoctor(doctors);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_schedule);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapterDoctor);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -91,16 +97,7 @@ public class ScheduleFragment extends android.support.v4.app.Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
