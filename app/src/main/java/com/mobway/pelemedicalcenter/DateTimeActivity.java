102c19f972edd6cf3e1297435a7dd06d42f2daf9
arthurcordova <arthur.stapassoli@gmail.com>
2017-11-21 15:54:49 -0200

    Patient List

--- app/src/main/java/com/mobway/pelemedicalcenter/DateTimeActivity.java
+++ app/src/main/java/com/mobway/pelemedicalcenter/DateTimeActivity.java
@@ -1,13 +1,21 @@
 package com.mobway.pelemedicalcenter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class DateTimeActivity extends AppCompatActivity {
+
+    private Button mButtonDate;
+    private Button mButtonNext;
+    private CalendarView mCalendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +23,26 @@ public class DateTimeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_date_time);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mButtonDate = findViewById(R.id.button_date);
+        mButtonNext = findViewById(R.id.button_next);
+        mCalendarView = findViewById(R.id.calendar_view);
+
+        mButtonDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DateTimeActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mButtonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getBaseContext(), PatientListActivity.class);
+                startActivity(it);
+            }
+        });
     }
 
 }
